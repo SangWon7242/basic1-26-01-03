7b0d8ef092d6539e3c1aca65de255511c05be7ef
SangWon7242 <gotkdqja@gmail.com>
2026-01-04 12:33:28 +0900

    ste2: 여러가지 리턴 타입

--- src/main/java/com/sbs/app1/domain/home/controller/HomeController.java
+++ src/main/java/com/sbs/app1/domain/home/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.sbs.app1.domain.home.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -95,7 +98,7 @@ public class HomeController {
   }
 
   @GetMapping("/home/returnMap")
-  public  Map<String, Object> showReturnMap() {
+  public Map<String, Object> showReturnMap() {
     /*
     Map<String, Object> map = Map.of(
         "name", "홍길동",
@@ -114,10 +117,206 @@ public class HomeController {
       put("address", "평택시");
       put("isMarried", true);
       put("phone", "010-1234-5678");
-      put( "email", "user1@test.com");
+      put("email", "user1@test.com");
       put("hobbies", List.of("수영", "테니스", "축구"));
     }};
 
     return map;
   }
+
+  @GetMapping("/home/returnMember")
+  public Member showReturnMember() {
+    Member member = new Member(
+        "홍길동",
+        30,
+        "평택시",
+        true,
+        "010-1234-5678",
+        "user1@test.com",
+        List.of("수영", "테니스", "축구")
+    );
+
+    return member;
+  }
+
+  @GetMapping("/home/returnMember2")
+  public Member showReturnMember2() {
+    Member member2 = new Member(
+        "홍길동",
+        30,
+        "평택시",
+        true,
+        "010-1234-5678",
+        "user1@test.com",
+        List.of("수영", "테니스", "축구")
+    );
+
+    return member2;
+  }
+
+  @GetMapping("/home/returnMemberList")
+  public List<Map<String, Object>> showReturnMemberList() {
+    Map<String, Object> member1 = new LinkedHashMap<>() {{
+      put("name", "홍길동");
+      put("age", 30);
+      put("address", "평택시");
+      put("isMarried", true);
+      put("phone", "010-1234-5678");
+      put("email", "user1@test.com");
+      put("hobbies", List.of("수영", "테니스", "축구"));
+    }};
+
+    Map<String, Object> member2 = new LinkedHashMap<>() {{
+      put("name", "홍길순");
+      put("age", 32);
+      put("address", "군포시");
+      put("isMarried", false);
+      put("phone", "010-1234-5678");
+      put("email", "user2@test.com");
+      put("hobbies", List.of("런닝", "헬스", "배구"));
+    }};
+
+    Map<String, Object> member3 = new LinkedHashMap<>() {{
+      put("name", "임꺽정");
+      put("age", 27);
+      put("address", "안산시");
+      put("isMarried", false);
+      put("phone", "010-1234-5678");
+      put("email", "user3@test.com");
+      put("hobbies", List.of("배구", "야구", "럭비"));
+    }};
+
+    List<Map<String, Object>> memberList = new ArrayList<>();
+    memberList.add(member1);
+    memberList.add(member2);
+    memberList.add(member3);
+
+    return memberList;
+  }
+
+  @GetMapping("/home/returnMemberList2")
+  public List<Member2> showReturnMemberList2() {
+    List<Member2> memberList = new ArrayList<>();
+    memberList.add(
+        new Member2("홍길동", 30,
+            "평택시", true,
+            "010-1234-5678", "user1@test.com",
+            List.of("수영", "테니스", "축구")));
+
+    memberList.add(
+        new Member2("홍길순", 32,
+            "안산시", false,
+            "010-1234-5678", "user3@test.com",
+            List.of("런닝", "헬스", "배구")));
+
+    memberList.add(
+        new Member2("임꺽정", 27,
+            "평택시", true,
+            "010-1234-5678", "user1@test.com",
+            List.of("배구", "야구", "럭비")));
+
+    return memberList;
+  }
+}
+
+class Member {
+  private String name;
+  private int age;
+  private String address;
+  private boolean isMarried;
+  private String phone;
+  private String email;
+  private List<String> hobbies;
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public boolean isMarried() {
+    return isMarried;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public List<String> getHobbies() {
+    return hobbies;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setMarried(boolean married) {
+    isMarried = married;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setHobbies(List<String> hobbies) {
+    this.hobbies = hobbies;
+  }
+
+  public Member(String name, int age, String address, boolean isMarried, String phone, String email, List<String> hobbies) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+    this.isMarried = isMarried;
+    this.phone = phone;
+    this.email = email;
+    this.hobbies = hobbies;
+  }
+
+  @Override
+  public String toString() {
+    return "Member{" +
+        "name='" + name + '\'' +
+        ", age=" + age +
+        ", address='" + address + '\'' +
+        ", isMarried=" + isMarried +
+        ", phone='" + phone + '\'' +
+        ", email='" + email + '\'' +
+        ", hobbies=" + hobbies +
+        '}';
+  }
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+class Member2 {
+  private String name;
+  private int age;
+  private String address;
+  private boolean isMarried;
+  private String phone;
+  private String email;
+  private List<String> hobbies;
 }
