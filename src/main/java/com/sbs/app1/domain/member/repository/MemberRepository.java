07691857ada8fb009f62e71e9c0c7703231c9f09
SangWon7242 <gotkdqja@gmail.com>
2026-01-04 15:43:42 +0900

    work

---
+++ src/main/java/com/sbs/app1/domain/member/repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package com.sbs.app1.domain.member.repository;
+
+import com.sbs.app1.domain.member.entity.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberRepository {
+  private List<Member> members;
+
+  public MemberRepository() {
+    members = new ArrayList<>();
+
+    for (int i = 1; i <= 10; i++) {
+      String username = "user" + i;      // 예: user1, user2 ...
+      String password = "1234" + i;      // 예: 12341, 12342 ...
+
+      members.add(new Member(username, password));
+    }
+  }
+
+  public Member findByUsername(String username) {
+    return members.stream()
+        .filter(member -> member.getUsername().equals(username))
+        .findFirst()
+        .orElse(null);
+  }
+}
--- src/main/java/com/sbs/app1/domain/member/service/MemberService.java
+++ src/main/java/com/sbs/app1/domain/member/service/MemberService.java
@@ -1,17 +1,29 @@
 package com.sbs.app1.domain.member.service;
 
+import com.sbs.app1.domain.member.entity.Member;
+import com.sbs.app1.domain.member.repository.MemberRepository;
 import com.sbs.app1.global.base.rsData.RsData;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
 @Service
 public class MemberService {
-  public RsData tryLogin(String username, String password) {
+  private MemberRepository memberRepository;
 
-    if(!password.equals("1234")) {
-      return RsData.of("F-1", "비밀번호가 일치하지 않습니다.");
-    }
-    else if(!username.equals("user1")) {
+  public MemberService() {
+    memberRepository = new MemberRepository();
+  }
+
+  public RsData tryLogin(String username, String password) {
+    Member member = memberRepository.findByUsername(username);
+
+    if(member == null) {
       return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다.".formatted(username));
+    }
+
+    if(!member.getPassword().equals(password)) {
+      return RsData.of("F-1", "비밀번호가 일치하지 않습니다.");
     }
 
     return RsData.of("S-1", "'%s'님 환영합니다.".formatted(username));
