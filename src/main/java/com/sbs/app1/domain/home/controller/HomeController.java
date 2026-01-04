9e9ec24e7e418e47b9407e67c83a079ed03e968e
SangWon7242 <gotkdqja@gmail.com>
2026-01-04 12:20:51 +0900

    ste1: 여러가지 리턴 타입

--- src/main/java/com/sbs/app1/domain/home/controller/HomeController.java
+++ src/main/java/com/sbs/app1/domain/home/controller/HomeController.java
@@ -1,54 +1,45 @@
 package com.sbs.app1.domain.home.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller // @Controller : 스프링부트한테 해당 클래스는 컨트롤러의 역할이라고 알려줌
+import java.util.*;
+
+@RestController // @Controller + @ResponseBody
 public class HomeController {
   public int val = -1;
 
   @GetMapping("/")
-  @ResponseBody // 응답한 결과물을 body 에 보냄
   public String main() {
     return "Hello!";
   }
 
   @GetMapping("/home/main1")
-  @ResponseBody
   public String home1() {
     return "안녕하세요.";
   }
 
   @GetMapping("/home/main2")
-  @ResponseBody
   public String home2() {
     return "반갑습니다.";
   }
 
   @GetMapping("/home/increase")
-  @ResponseBody
   public int showIncrease() {
     return ++val;
   }
 
   @GetMapping("/home/decrease")
-  @ResponseBody
   public int showDecrease() {
     return --val;
   }
 
   @GetMapping("/home/initNumber")
-  @ResponseBody
   public String initNumber() {
     val = -1;
     return "값을 초기화 합니다.";
   }
 
   @GetMapping("/home/plus")
-  @ResponseBody
   // 스프링부트는 쿼리스트링의 파라미터를 함수 매개변수로 받을 수 있다.
   // @RequestParam 생략이 가능하다.
   // 파라미터의 기본값이 있는 경우 @RequestParam 생략이 불가능하다.
@@ -60,8 +51,73 @@ public class HomeController {
 
   // /home/plus2/1/2
   @GetMapping("/home/plus2/{a}/{b}")
-  @ResponseBody
   public int showPlus2(@PathVariable int a, @PathVariable int b) {
     return a + b;
+  }
+
+  @GetMapping("/home/returnBoolean")
+  public boolean showReturnBoolean() {
+    return true;
+  }
+
+  @GetMapping("/home/returnDouble")
+  public double showReturnDouble() {
+    return Math.PI;
+  }
+
+  @GetMapping("/home/returnArray")
+  public int[] showReturnArray() {
+    int[] arr = {10, 20, 30, 40, 50};
+    return arr;
+  }
+
+  @GetMapping("/home/returnList")
+  public List<Integer> showReturnList() {
+    // List<Integer> arr = List.of(10, 20, 30, 40, 50);
+
+    /*
+    List<Integer> arr = new ArrayList<>() {{
+      add(10);
+      add(20);
+      add(30);
+      add(40);
+      add(50);
+    }};
+    */
+    List<Integer> arr = new ArrayList<>();
+    arr.add(10);
+    arr.add(20);
+    arr.add(30);
+    arr.add(40);
+    arr.add(50);
+
+    return arr;
+  }
+
+  @GetMapping("/home/returnMap")
+  public  Map<String, Object> showReturnMap() {
+    /*
+    Map<String, Object> map = Map.of(
+        "name", "홍길동",
+        "age", 30,
+        "address", "평택시",
+        "isMarried", true,
+        "phone", "010-1234-5678",
+        "email", "user1@test.com",
+        "hobbies", List.of("수영", "테니스", "축구")
+    );
+    */
+
+    Map<String, Object> map = new LinkedHashMap<>() {{
+      put("name", "홍길동");
+      put("age", 30);
+      put("address", "평택시");
+      put("isMarried", true);
+      put("phone", "010-1234-5678");
+      put( "email", "user1@test.com");
+      put("hobbies", List.of("수영", "테니스", "축구"));
+    }};
+
+    return map;
   }
 }
